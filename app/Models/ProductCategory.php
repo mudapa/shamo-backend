de2bf06bf19a5737e2b7f718093825f7dbbee85c
mudapa <mudanpa@gmail.com>
2022-09-30 14:49:42 +0700

    API

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ProductCategory extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'categories_id', 'id');
+    }
+}
